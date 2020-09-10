eba04fa3e9355622e4a0533ca7f57fa46dff521e
Nikita Maralov <blacknikboard@gmail.com>
2020-09-10 22:50:51 +0300

    model initial

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/blackNIKboard/Jefferson-Cylinder/modules"
+	"github.com/blackNIKboard/Jefferson-Cylinder/models"
 )
 
 func main() {
--- models/go.mod
+++ models/go.mod
@@ -1,3 +1,3 @@
-module github.com/blackNIKboard/Jefferson-Cylinder/modules
+module github.com/blackNIKboard/Jefferson-Cylinder/models
 
 go 1.14
